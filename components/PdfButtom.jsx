4fdf895fb376f930af3e78e87f86128d1bba295c
refdd <titorefat76@example.com>
2023-02-08 04:31:09 +0200

    add pdf

--- components/PdfButtom.jsx
+++ components/PdfButtom.jsx
@@ -3,14 +3,17 @@ function PdfButtom() {
   // Function will execute on click of button
   const onButtonClick = () => {
     // using Java Script method to get PDF file
-    fetch("mohamedRefat.pdf").then((response) => {
+    fetch(
+      "Mohamed Refat_FrontendDevelper_React.js __ React Native - CV.pdf"
+    ).then((response) => {
       response.blob().then((blob) => {
         // Creating new object of PDF file
         const fileURL = window.URL.createObjectURL(blob);
         // Setting various property values
         let alink = document.createElement("a");
         alink.href = fileURL;
-        alink.download = "mohamedRefat.pdf";
+        alink.download =
+          "Mohamed Refat_FrontendDevelper_React.js __ React Native - CV.pdf";
         alink.click();
       });
     });
